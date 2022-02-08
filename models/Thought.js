ca996ea6a73c33b99fbfa483e0a1b9b92b60ef84
Jasper Oudemans <jasperoudemans@gmail.com>
2022-02-08 15:25:15 -0700

    models finished

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,19 @@
 const mongoose = require("mongoose");
 
+const reactionSchema = new mongoose.Schema({
+  reactionId: {
+    type: mongoose.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: { type: String, required: true, maxLength: 280 },
+  username: { type: String, required: true },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => new Date(timestamp).toLocaleString(),
+  },
+});
+
 const thoughtSchema = new mongoose.Schema({
   thoughtText: { type: String, required: true, maxLength: 280, minlength: 1 },
   createdAt: {
@@ -7,4 +21,13 @@ const thoughtSchema = new mongoose.Schema({
     default: Date.now,
     get: (timestamp) => new Date(timestamp).toLocaleString(),
   },
+  username: { type: String, required: true },
+  reactions: [reactionSchema],
 });
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("Thought", thoughtSchema);
+
+module.exports = { thoughtSchema, Thought };
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { thoughtSchema } = require("./Thought");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true, trim: true },
@@ -14,7 +15,10 @@ const userSchema = new mongoose.Schema({
   thoughts: [thoughtSchema],
   friends: [userSchema],
 });
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+module.exports = { User };
